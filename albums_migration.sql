8039d5e05efebd19b1d86582fd0b7b176e15360b
Justin English <justin.english39@gmail.com>
2022-06-29 15:47:49 -0500

    FEAT : start of Seeders Lesson, Having weird parsing error, Fixed minor issues with migrations file

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED AUTO_INCREMENT,
     album_name VARCHAR(50) DEFAULT 'album Name not found',
-    release_date DATE NOT NULL,
+    release_date INT DEFAULT 0,
     artist VARCHAR(50) DEFAULT 'artist not found',
-    total_album_sales FLOAT DEFAULT 0,
+    total_album_sales FLOAT DEFAULT 0 ,
     genre VARCHAR(200) DEFAULT 'genre not found',
     PRIMARY KEY (id)
 );
+
---
+++ albums_seeder.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+INSERT INTO albums (album_name, total_album_sales)
+VALUES ('RockAndRoll' , 30),
+       ('RockAndStone' , 500),
+       ('ChristianRock' , 400);
