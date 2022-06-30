6742b3eb77f7bf141ab24e4d0556e9dc27c675de
Justin English <justin.english39@gmail.com>
2022-06-30 09:46:47 -0500

    FEAT : Added and completed Books seeder/Migration warmup exercise

---
+++ books_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id INT UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+    date_published VARCHAR(20),
+    description VARCHAR(250) DEFAULT 'No Description was set',
+    bestseller_weeks INT UNSIGNED DEFAULT 0,
+    primary key (id)
+);
