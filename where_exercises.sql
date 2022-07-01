038a7424f4e49bb58bdc1b0dbe5d4c7788cc2b98
Justin English <justin.english39@gmail.com>
2022-07-01 10:20:44 -0500

    FEAT : PART 2 to where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,11 +3,11 @@ USE employees;
 
 --  Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 
-    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = ('M');
 
 --  Find all employees whose last name starts with 'E' — 7,330 rows.
 
-SELECT * FROM employees WHERE last_name LIKE ('E%');
+SELECT * FROM employees WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
 --  Find all employees with a 'q' in their last name — 1,873 rows.
 
-SELECT * FROM employees WHERE last_name like ('q');
+SELECT * FROM employees WHERE last_name LIKE ('q') AND last_name NOT IN ('%qu%');
